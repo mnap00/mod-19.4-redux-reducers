d845e71ee675e0521bd7852440622e99055fed6c
mnap <learn@10g.pl>
2018-05-13 22:27:23 +0200

    Add reducers

--- src/reducers.js
+++ src/reducers.js
@@ -1,5 +1,13 @@
+import {
+    ADD_COMMENT,
+    EDIT_COMMENT,
+    REMOVE_COMMENT,
+    THUMB_DOWN_COMMENT,
+    THUMB_UP_COMMENT
+} from 'actions';
+
 const initialState = {
-    comments: [];
+    comments: [],
     users: []
 };
 
@@ -15,7 +23,52 @@ function reducer(state = initialState, action) {
                     },
                     ...state
                 ]
-            })
+            });
+        case EDIT_COMMENT:
+            return Object.assign({}, state, {
+                comments: [
+                    state.comments.filter(
+                        comment => comment.id !== action.id
+                    ),
+                    {
+                        id: action.id,
+                        text: action.text,
+                        votes: 0
+                    }
+                ]
+            });
+        case REMOVE_COMMENT:
+            return Object.assign({}, state, {
+                comments: state.comments.filter(
+                    comment => comment.id !== action.id
+                )
+            });
+        case THUMB_DOWN_COMMENT:
+            return Object.assign({}, state, {
+                comments: state.comments.map((comment, id) => {
+                    if (id === action.id) {
+                        return Object.assign({}, comment, {
+                            id: comment.id,
+                            text: comment.text,
+                            votes: comment.votes - 1
+                        });
+                    }
+                    return comment;
+                })
+            });
+        case THUMB_UP_COMMENT:
+            return Object.assign({}, state, {
+                comments: state.comments.map((comment, id) => {
+                    if (id === action.id) {
+                        return Object.assign({}, comment, {
+                            id: comment.id,
+                            text: comment.text,
+                            votes: comment.votes + 1
+                        });
+                    }
+                    return comment;
+                })
+            });
         default:
             return state;
     }
