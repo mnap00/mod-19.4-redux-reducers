5654cfda6bf0b161f6d4e6fd37342789e86abdb1
mnap <learn@10g.pl>
2018-05-13 23:11:39 +0200

    Update reducers: add imports and combine sub-reducers

--- src/reducers.js
+++ src/reducers.js
@@ -1,75 +1,17 @@
-import {
-    ADD_COMMENT,
-    EDIT_COMMENT,
-    REMOVE_COMMENT,
-    THUMB_DOWN_COMMENT,
-    THUMB_UP_COMMENT
-} from 'actions';
+import {combineReducers} from 'redux';
+import comments from './comments';
+import users from './users';
 
 const initialState = {
     comments: [],
     users: []
 };
 
-function reducer(state = initialState, action) {
-    switch(action.type) {
-        case ADD_COMMENT:
-            return Object.assign({}, state, {
-                comments: [
-                    {
-                        id: action.id,
-                        text: action.text,
-                        votes: 0
-                    },
-                    ...state
-                ]
-            });
-        case EDIT_COMMENT:
-            return Object.assign({}, state, {
-                comments: [
-                    state.comments.filter(
-                        comment => comment.id !== action.id
-                    ),
-                    {
-                        id: action.id,
-                        text: action.text,
-                        votes: 0
-                    }
-                ]
-            });
-        case REMOVE_COMMENT:
-            return Object.assign({}, state, {
-                comments: state.comments.filter(
-                    comment => comment.id !== action.id
-                )
-            });
-        case THUMB_DOWN_COMMENT:
-            return Object.assign({}, state, {
-                comments: state.comments.map((comment, id) => {
-                    if (id === action.id) {
-                        return Object.assign({}, comment, {
-                            id: comment.id,
-                            text: comment.text,
-                            votes: comment.votes - 1
-                        });
-                    }
-                    return comment;
-                })
-            });
-        case THUMB_UP_COMMENT:
-            return Object.assign({}, state, {
-                comments: state.comments.map((comment, id) => {
-                    if (id === action.id) {
-                        return Object.assign({}, comment, {
-                            id: comment.id,
-                            text: comment.text,
-                            votes: comment.votes + 1
-                        });
-                    }
-                    return comment;
-                })
-            });
-        default:
-            return state;
-    }
+function app(state = initialState, action) {
+    return {
+        comments: comments(state.comments, action),
+        users: users(state.users, action)
+    };
 }
+
+export default app;
